bbb2b0471388f99d65d3432198cf85b97997b072
doddy-s <doddy.s@students.amikom.ac.id>
2024-07-12 14:18:14 +0700

    chore: project initialization

---
+++ src/main/java/com/doddysujatmiko/cicdtest/controllers/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.doddysujatmiko.cicdtest.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloWorld {
+    @GetMapping("/")
+    public String getHello() {
+        return "Hello there, the cicd works.";
+    }
+}
