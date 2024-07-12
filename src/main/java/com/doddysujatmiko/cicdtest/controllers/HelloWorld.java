a2152522771783e59b3cfb212d78df08b6e47d20
doddy-s <doddy.s@students.amikom.ac.id>
2024-07-12 16:27:44 +0700

    test: test if cicd works as intended

--- src/main/java/com/doddysujatmiko/cicdtest/controllers/HelloWorld.java
+++ src/main/java/com/doddysujatmiko/cicdtest/controllers/HelloWorld.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
     @GetMapping("/")
     public String getHello() {
-        return "Hello there, the cicd works.";
+        return "Hello there, the cicd works. This is the first test of update.";
     }
 }
